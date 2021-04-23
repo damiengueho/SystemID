fd08951998ab619a65061e0ed15ea6496c4a8c14
Damien <damien.gueho@gmail.com>
2021-04-23 08:15:07 -0400

    v10

---
+++ ClassesGeneral/ClassSignal.py
@@ -0,0 +1,149 @@
+"""
+Author: Damien GUEHO
+Copyright: Copyright (C) 2021 Damien GUEHO
+License: Public Domain
+Version: 10
+Date: April 2021
+Python: 3.7.7
+"""
+
+
+import numpy as np
+import scipy.linalg as LA
+
+from SystemIDAlgorithms.Propagation import propagation
+
+
+
+class Signal:
+    def __init__(self, dimension, name):
+        self.dimension = dimension
+        self.name = name
+
+
+
+class DiscreteSignal(Signal):
+    def __init__(self, dimension, name, total_time, frequency, **kwargs):
+        super().__init__(dimension, name)
+        self.total_time = total_time
+        self.frequency = frequency
+        self.dt = 1/frequency
+        self.number_steps = int(self.total_time * self.frequency) + 1
+        self.signal_type = 'Discrete'
+        self.signal_shape = kwargs.get('signal_shape', 'Zero')
+
+        if self.signal_shape == 'White Noise':
+            self.mean = kwargs.get('mean', np.zeros(self.dimension))
+            self.covariance = kwargs.get('covariance', np.eye(self.dimension))
+            self.data = np.matmul(LA.sqrtm(self.covariance), np.random.randn(self.dimension, self.number_steps)) + self.mean[:, np.newaxis]
+        elif self.signal_shape == 'Pulse':
+            self.magnitude_pulse = kwargs.get('magnitude_pulse', np.ones(self.dimension))
+            self.data = np.zeros([self.dimension, self.number_steps])
+            self.data[:, 0] = self.magnitude_pulse
+        elif self.signal_shape == 'Sinusoid':
+            self.magnitude_sinusoid = kwargs.get('magnitude_sinusoid', np.ones(self.dimension))
+            self.frequency_sinusoid = kwargs.get('frequency_sinusoid', np.ones(self.dimension))
+            self.phase_sinusoid = kwargs.get('phase_sinusoid', np.zeros(self.dimension))
+            self.data = np.zeros([self.dimension, self.number_steps])
+            for i in range(self.dimension):
+                self.data[i, :] = self.magnitude_sinusoid[i] * np.sin(2 * np.pi * self.frequency_sinusoid[i] * np.linspace(0, self.total_time, self.number_steps) + self.phase_sinusoid[i])
+        elif self.signal_shape == 'Triangle':
+            self.magnitude_peak = kwargs.get('magnitude_peak', np.ones(self.dimension))
+            self.data = np.zeros([self.dimension, self.number_steps])
+            for i in range(self.dimension):
+                self.data[i, 0:int(self.number_steps / 2) + 1] = np.linspace(0, self.magnitude_peak[i], int(self.number_steps / 2) + 1)
+                self.data[i, int(self.number_steps / 2):self.number_steps] = np.linspace(self.magnitude_peak[i], 0, self.number_steps - int(self.number_steps / 2))
+        elif self.signal_shape == 'Combination':
+            self.maximum_ramp = kwargs.get('maximum_ramp', np.ones(self.dimension))
+            self.exponential_decay_rate = kwargs.get('exponential_decay_rate', -np.ones(self.dimension))
+            self.data = np.zeros([self.dimension, self.number_steps])
+            for i in range(self.dimension):
+                self.data[i, 0:int(self.number_steps / 3)] = np.linspace(0, self.maximum_ramp[i], int(self.number_steps / 3))
+                self.data[i, int(self.number_steps / 3):2 * int(self.number_steps / 3)] = LA.sqrtm(self.covariance)[i, i] * np.random.randn(2 * int(self.number_steps / 3) - int(self.number_steps / 3)) + self.maximum_ramp[i]
+                self.data[i, 2 * int(self.number_steps / 3):self.number_steps] = self.data[i, 2 * int(self.number_steps / 3) - 1] * np.exp(self.exponential_decay_rate[i] * np.linspace(0,self.number_steps - 2 * int(self.number_steps / 3),self.number_steps - 2 * int(self.number_steps / 3)))
+        elif self.signal_shape == 'External':
+            data = kwargs.get('data', np.zeros([self.dimension, self.number_steps]))
+            if data.shape.__len__() == 1:
+                data = data.reshape(-1, data.shape[0])
+            self.data = data
+
+        else:
+            self.data = np.zeros([self.dimension, self.number_steps])
+
+
+
+
+class ContinuousSignal(Signal):
+    def __init__(self, dimension, name, **kwargs):
+        super().__init__(dimension, name)
+        self.signal_type = 'Continuous'
+        self.signal_shape = kwargs.get('signal_shape', 'Zero')
+
+        if self.signal_shape == 'White Noise':
+            self.mean = kwargs.get('mean', np.zeros(self.dimension))
+            self.covariance = kwargs.get('covariance', np.eye(self.dimension))
+            def u(t):
+                return np.matmul(LA.sqrtm(self.covariance), np.random.randn(self.dimension)) + self.mean
+            self.u = u
+        elif self.signal_shape == 'Pulse':
+            self.magnitude_pulse = kwargs.get('magnitude_pulse', np.ones(self.dimension))
+            def u(t):
+                if t == 0:
+                    out = self.magnitude_pulse
+                else:
+                    out = np.zeros(self.dimension)
+                return out
+            self.u = u
+        elif self.signal_shape == 'Sinusoid':
+            self.magnitude_sinusoid = kwargs.get('magnitude_sinusoid', np.ones(self.dimension))
+            self.frequency_sinusoid = kwargs.get('frequency_sinusoid', np.ones(self.dimension))
+            self.phase_sinusoid = kwargs.get('phase_sinusoid', np.zeros(self.dimension))
+            if self.dimension == 1:
+                def u(t):
+                    return self.magnitude_sinusoid * np.sin(2 * np.pi * self.frequency_sinusoid * t + self.phase_sinusoid)
+            else:
+                def u(t):
+                    data = self.magnitude_sinusoid.reshape((self.dimension, 1)) * np.sin(2 * np.pi * np.outer(self.frequency_sinusoid, t) + self.phase_sinusoid.reshape((self.dimension, 1)))
+                    if data.shape[1] == 1:
+                        return data[:, 0]
+                    else:
+                        return data
+            self.u = u
+        elif self.signal_shape == 'External':
+            def zero(t):
+                return np.zeros(self.dimension) * t
+            self.u = kwargs.get('u', zero)
+        else:
+            def zero(t):
+                return np.outer(np.zeros(self.dimension), t)
+            self.u = zero
+
+
+
+class OutputSignal(DiscreteSignal):
+    def __init__(self, signal, system, name, **kwargs):
+        if signal.signal_type == 'Discrete':
+            state_propagation = kwargs.get('state_propagation', False)
+            signal_input_history = kwargs.get('signal_input_history', False)
+            signal_output_history = kwargs.get('signal_output_history', False)
+            data = propagation(signal, system, state_propagation=state_propagation, signal_input_history=signal_input_history, signal_output_history=signal_output_history)
+            super().__init__(system.output_dimension, name, signal.total_time, signal.frequency, signal_shape='External', data=data[0])
+            self.state = data[1]
+        if signal.signal_type == 'Continuous':
+            tspan = kwargs.get('tspan', np.array([0, 0]))
+            total_time = tspan[-1]
+            number_steps = len(tspan)
+            frequency = int(round((number_steps - 1) / total_time))
+            data = propagation(signal, system, tspan=tspan)
+            super().__init__(system.output_dimension, name, total_time, frequency, signal_shape='External', data=data[0])
+            self.state = data[1]
+
+
+
+def subtract2Signals(signal1, signal2):
+    return DiscreteSignal(signal1.dimension, signal1.name + ' - ' + signal2.name, signal1.total_time, signal1.frequency, signal_shape='External', data=signal1.data-signal2.data)
+
+
+
+def add2Signals(signal1, signal2):
+    return DiscreteSignal(signal1.dimension, signal1.name + ' + ' + signal2.name, signal1.total_time, signal1.frequency, signal_shape='External', data=signal1.data+signal2.data)
---
+++ ClassesGeneral/ClassSystem.py
@@ -0,0 +1,58 @@
+"""
+Author: Damien GUEHO
+Copyright: Copyright (C) 2021 Damien GUEHO
+License: Public Domain
+Version: 10
+Date: April 2021
+Python: 3.7.7
+"""
+
+
+class System:
+    def __init__(self, state_dimension, input_dimension, output_dimension, initial_states, name):
+        self.state_dimension = state_dimension
+        self.input_dimension = input_dimension
+        self.output_dimension = output_dimension
+        self.initial_states = initial_states
+        self.x0 = self.initial_states[0][0]
+        self.name = name
+
+
+class DiscreteLinearSystem(System):
+    def __init__(self, frequency, state_dimension, input_dimension, output_dimension, initial_states, name, A, B, C, D):
+        super().__init__(state_dimension, input_dimension, output_dimension, initial_states, name)
+        self.frequency = frequency
+        self.dt = 1 / frequency
+        self.system_type = 'Discrete Linear'
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+
+class DiscreteNonlinearSystem(System):
+    def __init__(self, frequency, state_dimension, input_dimension, output_dimension, initial_states, name, F, G):
+        super().__init__(state_dimension, input_dimension, output_dimension, initial_states, name)
+        self.frequency = frequency
+        self.dt = 1 / frequency
+        self.system_type = 'Discrete Nonlinear'
+        self.F = F
+        self.G = G
+
+
+class ContinuousLinearSystem(System):
+    def __init__(self, state_dimension, input_dimension, output_dimension, initial_states, name, A, B, C, D):
+        super().__init__(state_dimension, input_dimension, output_dimension, initial_states, name)
+        self.system_type = 'Continuous Linear'
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+
+class ContinuousNonlinearSystem(System):
+    def __init__(self, state_dimension, input_dimension, output_dimension, initial_states, name, F, G):
+        super().__init__(state_dimension, input_dimension, output_dimension, initial_states, name)
+        self.system_type = 'Continuous Nonlinear'
+        self.F = F
+        self.G = G
